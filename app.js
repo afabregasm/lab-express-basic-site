2b9a41a07ea26e635695a3de8d47bd83e5c7c96f
Andrea Fàbregas Martínez <a.fabregas@outlook.es>
2022-06-21 22:12:08 +0200

    Basic iterations achieved but having issues with linkng styles and images.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (req, res) => {
+    res.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/gallery", (req, res) => {
+    res.sendFile(__dirname + "/views/gallery.html");
+});
